d186797d6ad395960cf176e3818d33870e725583
Mahmoud Elkalamawy <107044207+tawdebo@users.noreply.github.com>
2023-04-06 22:28:42 +0200

    Update moo.sh

--- moo.sh
+++ moo.sh
@@ -2,6 +2,8 @@
 /bin/kill -9 all
 cd ~/public_html/node-github-demo
 git pull origin master
+/home/devops/bin/npm -v 
+/home/devops/bin/node -v
 /home/devops/bin/npm install
 /home/devops/bin/npm build
 /home/devops/bin/pm2 restart restart all
